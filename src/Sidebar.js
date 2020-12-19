f8791b104fa917d960bd6af70d58e9387bda1ce9
Swanand-Wagh <swanandwagh7@gmail.com>
2020-12-19 17:36:07 +0530

    Initial commit.

---
+++ src/Sidebar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import SearchOutlined from "@material-ui/icons/SearchOutlined";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="header">
+        <Avatar />
+        <div className="right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="search">
+        <div className="wrapper">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="chats"></div>
+    </div>
+  );
+}
+
+export default Sidebar;
