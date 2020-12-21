612f2e44f0cd441d72545e3915b5e0a8c4c5f1fd
Swanand-Wagh <swanandwagh7@gmail.com>
2020-12-21 15:39:50 +0530

    Chat body & footer ready.

--- src/Chat.js
+++ src/Chat.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Chat.css";
 import { Avatar, IconButton } from "@material-ui/core";
 import SearchOutlined from "@material-ui/icons/SearchOutlined";
-import { MoreVert } from "@material-ui/icons";
+import { AttachFile, InsertEmoticon, Mic, MoreVert } from "@material-ui/icons";
 
 function Chat() {
   return (
@@ -22,8 +22,28 @@ function Chat() {
           </IconButton>
         </div>
       </div>
-      <div className="chat_body"></div>
-      <div className="chat_footer"></div>
+      <div className="chat_body">
+        <p className={`chat_message ${true && "chat_receiver"}`}>
+          <span className="chat_name">Alias</span>
+          Hey Lotus!!!
+          <span className="timestamp">3:20 pm</span>
+        </p>
+      </div>
+      <div className="chat_footer">
+        <IconButton>
+          <InsertEmoticon />
+        </IconButton>
+        <IconButton>
+          <AttachFile />
+        </IconButton>
+        <form action="#">
+          <input type="text" placeholder="Type a message" />
+          <button type="submit">Send</button>
+        </form>
+        <IconButton>
+          <Mic />
+        </IconButton>
+      </div>
     </div>
   );
 }
