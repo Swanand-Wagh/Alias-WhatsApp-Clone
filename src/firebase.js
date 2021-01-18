a6f5dbd0666a3d9903ae61b8d9048c32eb24a5d4
Swanand-Wagh <swanandwagh7@gmail.com>
2021-01-19 02:22:35 +0530

    some minor changes

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,7 @@
 import firebase from "firebase";
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBUMJrIZhHE5e8EWZSXgr8tv4MsuLM382U",
-  authDomain: "aliass-whatsapp-clone.firebaseapp.com",
-  projectId: "aliass-whatsapp-clone",
-  storageBucket: "aliass-whatsapp-clone.appspot.com",
-  messagingSenderId: "952121283963",
-  appId: "1:952121283963:web:fb2989dab67988059419de",
-  measurementId: "G-36YK4Q7YTM",
-};
+import { firebaseConfig } from "./firebase-config.js";
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
